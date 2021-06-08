d53038bd58d5c5af006c2bce313b8088eed2b4c9
qixiaoling <x.qi@novi-education.nl>
2021-06-08 20:10:43 +0200

    initial commit to develop branch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,47 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react'
+import axios from "axios";
 import './App.css';
 
 function App() {
+    const [data, setData] = useState(null)
+
+    useEffect(() => {
+
+        async function getPokemon() {
+            try {
+                const result = await axios.get('https://pokeapi.co/api/v2/pokemon/jigglypuff')
+                console.log(result.data)
+                setData(result.data)
+
+            } catch(error) {
+                console.log(error)
+            }
+        }
+        getPokemon()
+
+    }, [])
+
     return (
-        <div>
-            Begin hier met de opdracht!
-        </div>
+        <>
+            <div>
+                <h1>Pokemon</h1>
+            </div>
+            <div>
+                <button>previous</button>
+                <button>next</button>
+            </div>
+            <div>
+                {data ? (
+                    <div>
+                        <h1>{data.name}</h1>
+                        <img src={data.sprites.front_default}/>
+                    </div>
+                ) : <h1>loading...</h1>}
+            </div>
+        </>
+
     );
 }
 
 export default App;
+
