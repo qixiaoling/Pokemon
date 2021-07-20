c9b1a8ec2f1f364c34ed4a7c7cd7146e064d1238
qixiaoling <x.qi@novi-education.nl>
2021-07-20 10:12:21 +0200

    child component

--- src/Component/Pokemon.js
+++ src/Component/Pokemon.js
@@ -23,10 +23,29 @@ function Pokemon({endPoint}){
     },[])
 
     return(
-        <>
-        <h1>{pokemon.name}</h1>
-
-        </>
+        <section className="poke-card">
+            {pokemon &&
+            <>
+                <h2>{pokemon.name}</h2>
+                <img
+                    alt="Afbeelding pokémon"
+                    src={pokemon.sprites.front_default}
+                />
+                <p><strong>Moves: </strong>{pokemon.moves.length}</p>
+                <p><strong>Weight: </strong>{pokemon.weight}</p>
+                <p><strong>Abilities: </strong></p>
+                <ul>
+                    {pokemon.abilities.map((ability) => {
+                        return (
+                            <li key={`${ability.ability.name}-${pokemon.name}`}>
+                                {ability.ability.name}
+                            </li>
+                        )
+                    })}
+                </ul>
+            </>
+            }
+        </section>
     )
 
 
